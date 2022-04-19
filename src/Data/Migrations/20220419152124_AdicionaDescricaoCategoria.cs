e6979788be2a334bd0e6866afc42123fb0ebcbfc
Luciana Reis <lucianareis113@gmail.com>
2022-04-19 12:24:04 -0300

    Adiciona descricao na categoria

---
+++ src/Data/Migrations/20220419152124_AdicionaDescricaoCategoria.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace loveladies.Data.Migrations
+{
+    public partial class AdicionaDescricaoCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mentoras_Categorias_CategoriaId",
+                table: "Mentoras");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Mentoras",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Categorias",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mentoras_Categorias_CategoriaId",
+                table: "Mentoras",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mentoras_Categorias_CategoriaId",
+                table: "Mentoras");
+
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Categorias");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Mentoras",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mentoras_Categorias_CategoriaId",
+                table: "Mentoras",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
